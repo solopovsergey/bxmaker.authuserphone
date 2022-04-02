1743988d97a7573b37788a234e1488c7f4df1ebd
Sergey Solopov <solopov.biz@gmail.com>
2022-04-02 17:11:03 +0300

    Добавлен пример обработки события перед регистрацией пользвоателя для расширения полей регистрации

---
+++ events/onRegisterExpandFields.php
@@ -0,0 +1,38 @@
+<?php
+
+// пример обработчика события регистрации пользвоателя,
+//  который дополнит поля пользователя дополнтельными данными
+// Используется при расширении полей регистраций компонентов на vue
+
+
+// разместить можно например в /bitrix/php_interface/init.php
+
+if (\Bitrix\Main\Loader::includeModule('bxmaker.authuserphone')) {
+
+    //регистрируем событие, которое также вызывает модуль перед добавлением пользователя
+    $eventManager = \Bitrix\Main\EventManager::getInstance();
+    $eventManager->addEventHandler(
+        "main",
+        "OnBeforeUserRegister",
+        "bxmaker_authuserphone_event_main_onBeforeUserRegister"
+    );
+
+
+    // непосредственно обработчик осбытия
+    function bxmaker_authuserphone_event_main_onBeforeUserRegister(&$arFields)
+    {
+        $req = \Bitrix\Main\Application::getInstance()->getContext()->getRequest();
+
+        // дополнитлеьные данные приходят в этом поле
+        $arExpandData = $req->getPost('expandData');
+
+        if (is_array($arExpandData) && isset($arExpandData['inviteCode'])) {
+            $arFields['UF_CODE'] = trim(htmlentities($arExpandData['inviteCode'], ENT_QUOTES));
+        }
+        else
+        {
+            throw new \Bxmaker\AuthUserPhone\Exception\BaseException('Не заполнено поле обязательное - Код приглашения', 'ERROR_INVITE_CODE');
+        }
+    }
+}
+
